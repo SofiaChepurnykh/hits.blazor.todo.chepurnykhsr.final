18cebf43d3cf1040442aeefdd122493b1de36ef1
SofiaChepurnykh <winxsofa@bk.ru>
2024-12-20 00:28:16 +0500

    4

--- TodoServerApp/Data/ApplicationDbContext.cs
+++ TodoServerApp/Data/ApplicationDbContext.cs
@@ -5,5 +5,17 @@ namespace TodoServerApp.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public virtual DbSet<EmployeeItem> EmployeeItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<EmployeeItem>().HasData([
+                new() { Id = 1, Name="Чепурных Софья", Position="Директор", Time=8, Salary=300000},
+            new() { Id = 2, Name="Шаукерова Аделия", Position="Разработчик", Time=8, Salary=150000},
+            new() { Id = 3, Name="Шананина Валерия", Position="Менеджер", Time=12, Salary=80000},
+            new() { Id = 4, Name="Косишнева Анастасия", Position="Бухгалтер", Time=12, Salary=80000},
+            ]);
+        }
     }
 }
---
+++ TodoServerApp/Data/Migrations/20241219191443_add_employeeitem.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TodoServerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_employeeitem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Time = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "EmployeeItems",
+                columns: new[] { "Id", "Name", "Position", "Salary", "Time" },
+                values: new object[,]
+                {
+                    { 1, "Чепурных Софья", "Директор", 300000, 8 },
+                    { 2, "Шаукерова Аделия", "Разработчик", 150000, 8 },
+                    { 3, "Шананина Валерия", "Менеджер", 80000, 12 },
+                    { 4, "Косишнева Анастасия", "Бухгалтер", 80000, 12 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeItems");
+        }
+    }
+}
