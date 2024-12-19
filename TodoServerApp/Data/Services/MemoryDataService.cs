bbe5d54d2c9f2b4a5a3724566b7fc33fb33a5e33
SofiaChepurnykh <winxsofa@bk.ru>
2024-12-19 23:53:44 +0500

    3

---
+++ TodoServerApp/Data/Services/MemoryDataService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using TodoServerApp.Components.Pages;
+using TodoServerApp.Data.Interfaces;
+
+namespace TodoServerApp.Data.Services
+{
+    public class MemoryDataService : IDataService
+    {
+        private static IEnumerable<EmployeeItem> Employees { get; } = [
+            new() { Id = 1, Name="Чепурных Софья", Position="Директор", Time=8, Salary=300000},
+            new() { Id = 2, Name="Шаукерова Аделия", Position="Разработчик", Time=8, Salary=150000},
+            new() { Id = 3, Name="Шананина Валерия", Position="Менеджер", Time=12, Salary=80000},
+            new() { Id = 4, Name="Косишнева Анастасия", Position="Бухгалтер", Time=12, Salary=80000},
+        ];
+
+        public async Task<IEnumerable<EmployeeItem>> GetEmployeeItemsAsync()
+        {
+            await Task.Delay(1000);
+            return await Task.FromResult(Employees);
+        }
+    }
+}
